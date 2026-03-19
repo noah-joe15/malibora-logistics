50dbb4efa30dd1db357b166eec40ebb6f6641226
Joram Noah Savant <joramnoah565@gmail.com>
2026-03-19 13:49:02 +0300

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,7 @@
-self.addEventListener('fetch', function(event) {});
+// In your app JS:
+navigator.serviceWorker.controller.postMessage({ type: 'FORCE_SYNC' });
+navigator.serviceWorker.controller.postMessage({ type: 'SKIP_WAITING' }); // for update prompts
+// With reply:
+const ch = new MessageChannel();
+ch.port1.onmessage = e => console.log('Queue:', e.data.count);
+navigator.serviceWorker.controller.postMessage({ type: 'GET_QUEUE_COUNT' }, [ch.port2]);
