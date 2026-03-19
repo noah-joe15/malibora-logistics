f5c2d9f2b2539308d765ea5ccfc6a7232e19cdb4
Joram Noah Savant <joramnoah565@gmail.com>
2026-03-19 15:36:35 +0000

    fix: move all frontend files to docs at root

--- public/sw.js
+++
@@ -1,7 +0,0 @@
-// In your app JS:
-navigator.serviceWorker.controller.postMessage({ type: 'FORCE_SYNC' });
-navigator.serviceWorker.controller.postMessage({ type: 'SKIP_WAITING' }); // for update prompts
-// With reply:
-const ch = new MessageChannel();
-ch.port1.onmessage = e => console.log('Queue:', e.data.count);
-navigator.serviceWorker.controller.postMessage({ type: 'GET_QUEUE_COUNT' }, [ch.port2]);
